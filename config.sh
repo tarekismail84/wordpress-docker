9608f8f66fc1f1dc342fbd866851e56d43eb64b5
Tarek Ismail <tismail@varrow.tech>
2022-07-14 14:45:59 +0200

    edit 2

--- config.sh
+++ config.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-#Change user to ec2-user#
-sudo su ec2-user
-
-#Install docker, run#
-sudo yum install docker -y
-
-
-#Add group membership for the default ec2-user so you can run all docker commands without using the sudo command#
-sudo usermod -a -G docker ec2-user
-id ec2-user
-
-# Reload a Linux user's group assignments to docker w/o logout#
-newgrp docker
-
-#docker-compose installation#
-wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
-sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
-sudo chmod -v +x /usr/local/bin/docker-compose
-
-##enable auto start and enable docker service##
-sudo systemctl enable --now docker.service
-
 
 
 #add docker compose path to all users#
